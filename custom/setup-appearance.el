12b039d445c78be0d264d351f1a7d0f1f683992f
Mykola Liubchak <mykolal@interfacemasters.com>
2018-08-02 16:38:49 +0300

    Init in full screen mode, nyan-mode from scratch

--- custom/setup-appearance.el
+++ custom/setup-appearance.el
@@ -4,7 +4,18 @@
 ;; enable linum mode by default
 (global-linum-mode t)
 
+;; enable column-number-mode
+(column-number-mode t)
+
 ;; main theme
 (load-theme 'material t)
+
+;; full screan mode
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;; load nyan mode
+(nyan-mode t)
+
 
 (provide 'setup-appearance)
